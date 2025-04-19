207b7b7e562f168d50f998c5bce250c9368d9b4c
Faraz5658 <farazahmad5623@gmail.com>
2025-04-19 11:23:06 +0500

    example

---
+++ Variables (var, let, const)/var/EXAMPLE -(1)/EXAMPLE -(4)/app.js
@@ -0,0 +1,14 @@
+
+var number = 6;
+console.log('number: ', number);
+
+var number = 5;
+var number = 8;
+console.log('number: ', number);
+
+
+var number = qwe;
+number = weqwe;
+console.log('number: ', number);
+
+
