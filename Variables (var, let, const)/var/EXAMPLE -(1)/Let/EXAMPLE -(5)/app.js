edccdbc7fc58dc067d32d455f174e183da62a3d6
Faraz5658 <farazahmad5623@gmail.com>
2025-04-20 22:19:25 +0500

    update

---
+++ Variables (var, let, const)/var/EXAMPLE -(1)/Let/EXAMPLE -(5)/app.js
@@ -0,0 +1,14 @@
+
+var number = 6;
+console.log('number: ', number);
+
+var number = 5;
+var number = 8;
+console.log('number: ', number);
+
+
+var number = 5;
+number = 50;
+console.log('number: ', number);
+
+
