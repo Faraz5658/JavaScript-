09a4ac25dc88bbf95ce498a2280333520b11ec5a
Faraz5658 <farazahmad5623@gmail.com>
2025-05-16 17:26:04 +0500

    Destructure from Array of Objects

---
+++ Basic Array Destructuring/Basic Object Destructuring/Destructure from Array of Objects/app.js
@@ -0,0 +1,8 @@
+const posts = [
+  { id: 1, title: "FARAZ" },
+  { id: 2, title: "BANGASH" }
+];
+
+const [{ title: title1 }, { title: title2 }] = posts;
+console.log(title1); 
+console.log(title2); 
